0bf6987f60428fcd17d33ce3796ad217ef500ad6
Joel Chigumba <joel.chigumba@dae.mn>
2025-06-18 20:53:36 +0200

    feat: updated app configs

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,8 +3,9 @@ import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
-import "react-native-reanimated";
 import { LogBox } from "react-native";
+import "react-native-reanimated";
+import "../global.css";
 
 import { tokenCache } from "@/lib/auth";
 
